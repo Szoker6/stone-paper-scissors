1f39d7f2b1122de5a8fb0397a752e3a559959c47
Dawid Margraf <David_neo1@wp.pl>
2019-03-01 02:21:02 +0100

    Add function adding results not yet working and add function that check who won

--- js/script.js
+++ js/script.js
@@ -1,64 +1,88 @@
 'use strict';
 
-var output = document.getElementById('output');
-var showNumberOfRounds = document.getElementById('showNumberOfRounds');
-var oudputComp = document.getElementById('outputComp');
+var welcomeMessage = document.getElementById('welcomeMessage');
 var showResults = document.getElementById('showResults');
+var showNumberOfRounds = document.getElementById('showNumberOfRounds');
+var showWhoWon = document.getElementById('showWhoWon');
+var wins = document.getElementById('wins');
+var draws = document.getElementById('draws');
+var loses = document.getElementById('loses');
+var games = document.getElementById('games');
 
-output.innerHTML = 'Witamy w grze kamień papier nożyce';
+startGame();
+
+function startGame() {
+    welcomeMessage.innerHTML = 'Witamy w grze kamień papier nożyce';
+    registerListeners();
+};
 
 function askHowManyRounds() {     
-    var numbersOfRounds = prompt('Ile rund?');      
-    return numbersOfRounds;        
+    var numbersOfRounds = prompt('Ile rund?');   
+    return numbersOfRounds;
 };
-
-beginButton.addEventListener('click', function() {      
-    showNumberOfRounds.innerHTML = 'ilość rund: ' + askHowManyRounds();
-    document.getElementById('stone').style.display = "block";
-    document.getElementById('paper').style.display = "block";
-    document.getElementById('scissors').style.display = "block";
-});  
-
-stone.addEventListener('click', function clickStone() {	
-        outputComp.innerHTML = 'Dawid:' +  ' Komputer: ' + '</br>' + 'Kamień ' + getRandomItemNumber();
-});
-
-paper.addEventListener('click', function clickPaper() {	
-        outputComp.innerHTML = 'Dawid:' +  ' Komputer: ' + '</br>' + 'Papier ' + getRandomItemNumber();
         
-});
-
-scissors.addEventListener('click', function clickScissors() {	
-        outputComp.innerHTML = 'Dawid:' +  ' Komputer: ' + '</br>' + 'Nożyczki ' + getRandomItemNumber();
-});
-
-function randomizeKomputer() {
-        var random = (Math.floor(Math.random()*3));
-        return random;
+function registerListeners() {
+    beginButton.addEventListener('click', function() {
+        var totalRoundsForGameAsString = askHowManyRounds();
+        var totalRoundsForGame = Number(totalRoundsForGameAsString);
+        showNumberOfRounds.innerHTML = 'ilość rund: ' + totalRoundsForGame;
+        document.getElementById('stone').style.display = "block";
+        document.getElementById('paper').style.display = "block";
+        document.getElementById('scissors').style.display = "block";
+    });
+    stone.addEventListener('click', function() {
+        var computerChoice = getRandomChoice();
+        var playerChoice = 'kamień';
+        showResults.innerHTML = 'Gracz:' +  ' Komputer: ' + '</br>' + playerChoice + ' ' + computerChoice;
+        var win = didYouWon(playerChoice, computerChoice);   
+        var update = updateResult(playerChoice, computerChoice);    
+    });
+    paper.addEventListener('click', function() {
+        var computerChoice = getRandomChoice();
+        var playerChoice = 'papier';
+        showResults.innerHTML = 'Gracz:' +  ' Komputer: ' + '</br>' + playerChoice + ' ' + computerChoice;
+        var win = didYouWon(playerChoice, computerChoice);  
+        var update = updateResult(playerChoice, computerChoice);  
+    });
+    scissors.addEventListener('click', function() {
+        var computerChoice = getRandomChoice();
+        var playerChoice = 'nożyczki';
+        showResults.innerHTML = 'Gracz:' +  ' Komputer: ' + '</br>' + playerChoice + ' ' + computerChoice;
+        var win = didYouWon(playerChoice, computerChoice);
+        var update = updateResult(playerChoice, computerChoice);    
+    });
 };
-var condition = randomizeKomputer();
-function convertItemNumberToItem(item, number) {
-        
-        if (condition === 0) {  
-                return 'Kamień';
-        } else if (condition === 1) {   
-                return 'Papier';      
-        } else { 
-                return 'Nożyczki'
-        }   
+
+function getRandomChoice() {
+        var randomNumber = (Math.floor(Math.random()*3));
+        var choices = ['kamień', 'papier', 'nożyczki'];
+        return choices[randomNumber];
 };
 
-var randomItemNumber = getRandomItemNumber();
-var vomputerItem = convertItemNumberToItem(randomItemNumber);
+function didYouWon(playerChoice, computerChoice) {
+    if ((playerChoice == 'kamień' && computerChoice == 'nożyczki') || (playerChoice == 'papier' && computerChoice == 'kamień') || (playerChoice == 'nożyczki' && computerChoice == 'papier')) {
+        showWhoWon.innerHTML = 'Wygrana!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';    
+    } else if ((playerChoice == 'kamień' && computerChoice == 'kamień') || (playerChoice == 'papier' && computerChoice == 'papier') || (playerChoice == 'nożyczki' && computerChoice == 'nożyczki')) {
+        showWhoWon.innerHTML = 'Remis!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';    
+    } else {
+        showWhoWon.innerHTML = 'Porażka!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';     
+    }
+};
+ 
+function updateResult(playerChoice, computerChoice) {
+    if(playerChoice < computerChoice){
+        var winValue = parseInt(wins.innerHTML) + 1;
+        wins.innerHTML = winValue;
 
-function didWon() {
-        if (computerThing == 'Kamień' && computerThing == 'Papier' && computerThing == 'Nożyczki') {
-                return 'Remis';
-        } else if (computerThing == 'Nożyczki' && computerThing == 'Papier' && computerThing == 'Kamień') {
-                return 'Wygrałeś'
-        } else {
-                return 'Pregrałeś'
-        }
-}
-var win = didWon();
-showResults.innerHTML = didWon();
+    } else if (playerChoice === computerChoice) {
+        var drawValue = parseInt(draws.innerHTML) + 1;
+        draws.innerHTML = drawValue;
+    } else {
+        var loseValue = parseInt(loses.innerHTML) + 1;
+        loses.innerHTML = loseValue;
+    }
+        var gameValue = parseInt(games.innerHTML) + 1;
+        games.innerHTML = gameValue;
+};
+
+
