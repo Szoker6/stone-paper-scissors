9345fb75b38073b7ffa889d6fe98ffdddff4a2f2
Dawid Margraf <David_neo1@wp.pl>
2019-03-02 23:21:27 +0100

    Add function hoverButtons, function EndGame and if in function askHowManyRounds

--- js/script.js
+++ js/script.js
@@ -4,41 +4,57 @@ var welcomeMessage = document.getElementById('welcomeMessage');
 var showResults = document.getElementById('showResults');
 var showNumberOfRounds = document.getElementById('showNumberOfRounds');
 var showWhoWon = document.getElementById('showWhoWon');
+
 var wins = document.getElementById('wins');
 var draws = document.getElementById('draws');
 var loses = document.getElementById('loses');
 var games = document.getElementById('games');
+var endOfGame = document.getElementById('endOfGame');
+var roundsCounter = document.getElementById('roundsCounter');
+var totalRoundsForGame;
+var gameValue
+
+
 
 startGame();
 
 function startGame() {
     welcomeMessage.innerHTML = 'Witamy w grze kamień papier nożyce';
     registerListeners();
+    
 };
-
-function askHowManyRounds() {     
+var numbersOfRounds;
+function askHowManyRounds() {    
     var numbersOfRounds = prompt('Ile rund?');   
-    return numbersOfRounds;
+    if (isNaN(numbersOfRounds) ||
+        numbersOfRounds <= 0) {
+                return alert('Podaj prawidłową wartość');
+        } else {
+                return numbersOfRounds;
+        }
 };
-        
+   
 function registerListeners() {
     beginButton.addEventListener('click', function() {
         var totalRoundsForGameAsString = askHowManyRounds();
-        var totalRoundsForGame = Number(totalRoundsForGameAsString);
+        totalRoundsForGame = Number(totalRoundsForGameAsString);
         showNumberOfRounds.innerHTML = 'ilość rund: ' + totalRoundsForGame;
         document.getElementById('stone').style.display = "block";
         document.getElementById('paper').style.display = "block";
-        document.getElementById('scissors').style.display = "block";
+        document.getElementById('scissors').style.display = "block";      
     });
     stone.addEventListener('click', function() {
-        choseAnItem('kamień')     
+        choseAnItem('kamień')   
+        hoverButtons();  
     });
     paper.addEventListener('click', function() {
-        choseAnItem('papier')  
+        choseAnItem('papier');
+        hoverButtons();  
     });
     scissors.addEventListener('click', function() {
-        choseAnItem('nożyczki')     
-    });
+        choseAnItem('nożyczki');
+        hoverButtons();     
+    });   
 };
 
 function getRandomChoice() {
@@ -53,13 +69,11 @@ function showResult(playerChoice, computerChoice) {
     } else if ((playerChoice == 'kamień' && computerChoice == 'kamień') || (playerChoice == 'papier' && computerChoice == 'papier') || (playerChoice == 'nożyczki' && computerChoice == 'nożyczki')) {
         showWhoWon.innerHTML = 'Remis!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';       
     } else {
-        showWhoWon.innerHTML = 'Porażka!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';
-           
+        showWhoWon.innerHTML = 'Porażka!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';       
     }
 };
- 
+
 function updateResult(playerChoice, computerChoice) {
-    console.log(playerChoice, computerChoice);
     if((playerChoice == 'kamień' && computerChoice == 'nożyczki') || (playerChoice == 'papier' && computerChoice == 'kamień') || (playerChoice == 'nożyczki' && computerChoice == 'papier')) {
         var winValue = parseInt(wins.innerHTML) + 1;
         wins.innerHTML = winValue;
@@ -70,8 +84,9 @@ function updateResult(playerChoice, computerChoice) {
         var loseValue = parseInt(loses.innerHTML) + 1;
         loses.innerHTML = loseValue;
     }
-        var gameValue = parseInt(games.innerHTML) + 1;
+        gameValue = parseInt(games.innerHTML) + 1;
         games.innerHTML = gameValue;
+        EndGame(totalRoundsForGame,  gameValue);       
 };
 
 function choseAnItem(playerChoice) {
@@ -81,3 +96,26 @@ function choseAnItem(playerChoice) {
     updateResult(playerChoice, computerChoice);  
 };
 
+function EndGame(totalRoundsForGame,  gameValue) {
+console.log(totalRoundsForGame,  gameValue); 
+console.log(totalRoundsForGame === gameValue, totalRoundsForGame == gameValue)
+    if (totalRoundsForGame ===  gameValue) {
+         endOfGame.innerHTML = 'Koniec gry aby rozpoczać od nowa odświerz stronę';
+    } else {
+        return endOfGame.innerHTML =  ''; 
+    }
+};
+
+function hoverButtons() {
+    if (totalRoundsForGame ===  gameValue) {
+        stone.style.display = "none";
+        paper.style.display = "none";
+        scissors.style.display = "none";
+        beginButton.style.display = "none";
+    } else {
+        stone.style.display = "block";
+        paper.style.display = "block";
+        scissors.style.display = "block";
+        beginButton.style.display = "block";
+    }
+};
