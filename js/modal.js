ebd5496f40750fd2fc0c1e49e71e1523e313c012
Dawid Margraf <David_neo1@wp.pl>
2019-04-24 16:50:01 +0200

    change modal, copy to js/script

--- js/modal.js
+++ js/modal.js
@@ -1,47 +1,47 @@
 "use strict";
-var modalControl = (function () {
+// var modalControl = (function () {
 
-    var showModal = function (event) {
-        modals.forEach(function (modal) {
-            modal.classList.remove("show");
-        });
-        document.querySelector("#modal-overlay").classList.add("show");
+//     var showModal = function (event) {
+//         modals.forEach(function (modal) {
+//             modal.classList.remove("show");
+//        });
+//         document.querySelector("#modal-overlay").classList.add("show");
 
-        if (typeof event === "string") {
-            return document.getElementById(event).classList.add('show')
-        }
-        event.preventDefault();
+//         if (typeof event === "string") {
+//             return document.getElementById(event).classList.add('show')
+//         }
+//         event.preventDefault();
 
-        var modalID = event.target.getAttribute('href');
-        document.querySelector(modalID).classList.add('show')
-    };
+//         var modalID = event.target.getAttribute('href');
+//         document.querySelector(modalID).classList.add('show')
+//     };
 
-    var modalLinks = document.querySelectorAll(".show-modal");
+//     var modalLinks = document.querySelectorAll(".show-modal");
 
-    for (var i = 0; i < modalLinks.length; i++) {
-        modalLinks[i].addEventListener("click", showModal);
-    }
+//     for (var i = 0; i < modalLinks.length; i++) {
+//         modalLinks[i].addEventListener("click", showModal);
+//     }
 
-    var hideModal = function (event) {
-        event.preventDefault();
-        document.querySelector("#modal-overlay").classList.remove("show");
-    };
+//     var hideModal = function (event) {
+//         event.preventDefault();
+//         document.querySelector("#modal-overlay").classList.remove("show");
+//     };
 
-    var closeButtons = document.querySelectorAll(".modal .close");
+//     var closeButtons = document.querySelectorAll(".modal .close");
 
-    for (var i = 0; i < closeButtons.length; i++) {
-        closeButtons[i].addEventListener("click", hideModal);
-    }
+//     for (var i = 0; i < closeButtons.length; i++) {
+//         closeButtons[i].addEventListener("click", hideModal);
+//     }
 
-    document
-        .querySelector("#modal-overlay")
-        .addEventListener("click", hideModal);
+//     document
+//         .querySelector("#modal-overlay")
+//         .addEventListener("click", hideModal);
 
-    var modals = document.querySelectorAll(".modal");
+//     var modals = document.querySelectorAll(".modal");
 
-    for (var i = 0; i < modals.length; i++) {
-        modals[i].addEventListener("click", function (event) {
-            event.stopPropagation();
-        });
-	}
-})();
+//     for (var i = 0; i < modals.length; i++) {
+//         modals[i].addEventListener("click", function (event) {
+//             event.stopPropagation();
+//         });
+// 	}
+// })();
--- js/script.js
+++ js/script.js
@@ -5,47 +5,53 @@ var showResults = document.getElementById('showResults');
 var showNumberOfRounds = document.getElementById('showNumberOfRounds');
 var showWhoWon = document.getElementById('showWhoWon');
 var endOfGameMessage = document.getElementById('endOfGameMessage');
-var whowon = document.getElementById('whowon');
+var winnerResult = document.getElementById('winnerResult');
 var beginButton = document.getElementById('beginButton');
-var modalResults = document.getElementById('modalResults');
 var winner = document.getElementById('wins');
 var remixed = document.getElementById('draws');
 var losser = document.getElementById('loss');
 var games = document.getElementById('games');
 
+// modal variables
+var modal = document.getElementById('modal-one');
+var modalOverlay = document.getElementById("modal-overlay");
+
 var totalRoundsForGame;
 var params = {
- progress = [],
- wins: 0,
- loss: 0,
- game: 0,
- draws: 0
+    progress: [
+        
+           
+        
+    ],
+    wins: 0,
+    loss: 0,
+    game: 0,
+    draws: 0
 }
 
 startGame();
-modalResults.style.display = "none";
 function startGame() {
     welcomeMessage.innerHTML = 'Witamy w grze kamień papier nożyce';
     registerListeners();
 };
 
-function askHowManyRounds() {    
-    var numbersOfRounds = prompt('Ile rund?');  
+function askHowManyRounds() {
+    var numbersOfRounds = prompt('Ile rund?');
     if (isNaN(numbersOfRounds) || numbersOfRounds <= 0) {
             alert('Podałeś nieprawidłową wartość licznik ustawiony na 5 rund');
-            return '5';       
+            return '5';
     } else {
             return numbersOfRounds;
-    }       
+    }
 };
-   
+
 function registerListeners() {
     beginButton.addEventListener('click', function() {
         hideStartGameButtons('none');
         var totalRoundsForGameAsString = askHowManyRounds();
         totalRoundsForGame = Number(totalRoundsForGameAsString);
         showNumberOfRounds.innerHTML = 'ilość rund: ' + totalRoundsForGame;
-        setButtonsDisplay('block');      
+        setButtonsDisplay('block');
     });
     var buttons = document.querySelectorAll('button');
     for (let i = 0; i < buttons.length; i++) {
@@ -55,13 +61,12 @@ function registerListeners() {
             choseAnItem(currentButtonName); // kamien, papier, nozyczki
         });
     }
-    
+    modalOverlay.addEventListener("click", hideModal);
 }
-    newGameButton.addEventListener('click', function() { 
+    newGameButton.addEventListener('click', function() {
         reset();
         newGameButton.style.display = "none";
-    });    
-
+    });
 
 function getRandomChoice() {
     var randomNumber = (Math.floor(Math.random()*3));
@@ -71,47 +76,39 @@ function getRandomChoice() {
 
 function showResult(playerChoice, computerChoice) {
     if ((playerChoice == 'kamień' && computerChoice == 'nożyczki') || (playerChoice == 'papier' && computerChoice == 'kamień') || (playerChoice == 'nożyczki' && computerChoice == 'papier')) {
-        showWhoWon.innerHTML = 'Wygrana!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';     
+        showWhoWon.innerHTML = 'Wygrana!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';
         params.wins = params.wins + 1 ;
         winner.innerHTML = params.wins;
-    } else if ((playerChoice == 'kamień' && computerChoice == 'kamień') || (playerChoice == 'papier' && computerChoice == 'papier') || (playerChoice == 'nożyczki' && computerChoice == 'nożyczki')) {       
-        showWhoWon.innerHTML = 'Remis!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';       
+    } else if ((playerChoice == 'kamień' && computerChoice == 'kamień') || (playerChoice == 'papier' && computerChoice == 'papier') || (playerChoice == 'nożyczki' && computerChoice == 'nożyczki')) {
+        showWhoWon.innerHTML = 'Remis!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';
         params.draws = params.draws + 1;
         remixed.innerHTML = params.draws;
-    } else {      
-        showWhoWon.innerHTML = 'Porażka!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';       
+    } else {
+        showWhoWon.innerHTML = 'Porażka!' + '<br/>' + 'Twój wybór: ' + playerChoice + '<br/>' + 'Wybór komputera: ' + computerChoice + '<br>';
         params.loss = params.loss + 1;
         losser.innerHTML = params.loss;
     }
     params.game = params.game + 1;
         games.innerHTML = params.game;
-        endGame(totalRoundsForGame, params.game);  
-};
-    function whoWon() {
-        if (params.wins > params.loss) {
-            whowon.innerHTML = 'wygrałes'
-        } else if (params.wins < params.loss) {
-            whowon.innerHTML = 'przegrałeś'
-        } else  whowon.innerHTML = 'remis'
-    };
-   
-function choseAnItem(playerChoice) {
-    var computerChoice = getRandomChoice(); 
-    showResults.innerHTML = 'Gracz:' +  ' Komputer: ' + '</br>' + playerChoice + ' ' + computerChoice;
-    beginButton.style.display = "none";   
-    showResult(playerChoice, computerChoice);  
+        checkIfEndGame(totalRoundsForGame, params.game);
 };
 
-function endGame(totalRoundsForGame, game) {  
+function choseAnItem(playerChoice) {
+    var computerChoice = getRandomChoice();
+    showResults.innerHTML = 'Gracz:' +  ' Komputer: ' + '</br>' + playerChoice + ' ' + computerChoice;
+    beginButton.style.display = "none";
+    showResult(playerChoice, computerChoice);
+};
+
+function checkIfEndGame(totalRoundsForGame, game) {
     if (totalRoundsForGame ===  game) {
         endOfGameMessage.innerHTML = 'Aby rozpoczać od nowa kliknij nowa gra';
-        setButtonsDisplay('none');   
-        newGameButton.style.display = "block";   
-        modalResults.style.display = "block";   
-    } else {  
-        endOfGameMessage.innerHTML =  ''; 
-    }  
-    createModal();
+        setButtonsDisplay('none');
+        newGameButton.style.display = "block";
+        createModal();
+    } else {
+        endOfGameMessage.innerHTML =  '';
+    }
 };
 
 function setButtonsDisplay(display) {
@@ -121,44 +118,59 @@ function setButtonsDisplay(display) {
 };
 
 function hideStartGameButtons(display) {
-    beginButton.style.display = display;   
-    newGameButton.style.display = display; 
-    modalResults.style.display = display; 
-    
+    beginButton.style.display = display;
+    newGameButton.style.display = display;
 };
 
-function reset() {   
-    showWhoWon.innerHTML = showResults.inner = endOfGameMessage.innerHTML ='';  
+function reset() {
+    showWhoWon.innerHTML = showResults.inner = endOfGameMessage.innerHTML ='';
     showNumberOfRounds.innerHTML = 'ilość rund: ';
     beginButton.style.display = "block";
-    updateView();     
+    updateView();
 };
 
 function updateView() {
     params.wins = 0;
     params.loss = 0;
     params.game = 0;
-    params.draws = 0;    
+    params.draws = 0;
     winner.innerHTML = params.wins;
     remixed.innerHTML = params.draws;
     losser.innerHTML = params.loss;
     games.innerHTML = params.game;
 };
 
-function whoWon() {
+function displayWinnerInModal() {
     if (params.wins > params.loss) {
-        whowon.innerHTML = 'wygrałes'
+        winnerResult.innerHTML = 'wygrałes';
     } else if (params.wins < params.loss) {
-        whowon.innerHTML = 'przegrałeś'
-    } else  whowon.innerHTML = 'remis'
+        winnerResult.innerHTML = 'przegrałeś';
+    } else {
+        winnerResult.innerHTML = 'remis';
+    }
 };
 
 function createModal() {
-    modalResults.style.display = "block";
+    displayWinnerInModal();
     var modal = document.getElementById('modal-one');
     var content = modal.querySelector('.content');
+    var resultList = modal.querySelector('.resultList');
     var p = document.createElement('p');
-    p.innerHTML = JSON.stringify(params);
-    content.appendChild(p);  
-    
+    var li = document.createElement('li');
+    li.innerHTML = `Number of rounds: ${params.game}`;
+   // p.innerHTML = 'W: ' + params.wins + ' vs ' + ' L: ' + params.loss;
+    p.innerHTML = `Win: ${params.wins} vs Loss: ${params.loss}`;
+    content.appendChild(p);
+    resultList.appendChild(li)
+    showModal();
 }
+
+function hideModal() {
+    modalOverlay.classList.remove("show");
+    modal.classList.remove("show");
+};
+
+function showModal() {
+    modalOverlay.classList.add("show");
+    modal.classList.add('show')
+};
